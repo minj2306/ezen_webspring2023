fc4fa082562ffbb8df200fc73be5c6999b6d941c
Pminjae <alswo2035@naver.com>
2023-10-05 14:07:49 +0900

    Todo Spring 정적파일 반환하는 방법

---
+++ src/main/java/day04/AppStart.java
@@ -0,0 +1,30 @@
+package day04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+    메타 어노테이션 ?? : 실행 또는 컴파일 했을때 사용방법(이미 설치된 라이브러리) 에 대해 정의
+*/
+@SpringBootApplication // @ComponentScan [ @Controller , @RestController , @Service , @Repository , @Configure 등록 ]
+// 모든 컴포넌트들을 찾아서 빈등록
+public class AppStart {
+
+    public static void main(String[] args) {
+        SpringApplication.run( AppStart.class );
+    }
+
+}
+
+/*
+    스프링이 view(정적) 파일들을 찾는 위치 resoursces 폴더
+        -주의할 점 : 본인이 만들고 싶은곳데 정적(view) 파일 만들면 안된다.
+    HTML : resources -> templates -> html 파일
+    JS/CSS/Image : resources -> static -> JS/CSS/Image 파일
+
+    - JSP 프로젝트와 SPRING 프로젝트의 정적파일 경로 차이
+        - JSP는 패키지의 경로와 파일명이 곧 URL 
+        
+        - SPRING 은 정적팡리 호출하는 URL 매핑
+            매핑후 Resource 반환
+*/
---
+++ src/main/java/day04/TodoController.java
@@ -0,0 +1,68 @@
+package day04;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+// 스프링 빈 : 스프링 컨테이너(저장소) 에 저장된 객체 [ 이유 : 스프링이 대신 객체 관리 => 여러 개발자들이 작업했을떄 기준 ]
+
+//@Controller // Spring MVC 중 해당 클래스를 Controller 로 사용 // 스프링 컨테이너 빈 등록
+@RestController // Controller + ResponseBody
+@RequestMapping("/todo")// HTTP 로 부터의 해당 클래스의 매핑 주소 만들기
+public class TodoController {
+
+    @Autowired
+    public TodoService todoService;
+
+    //REST : HTTP 기반으로 GET , POST , PUT , DELETE 메소드 이용한 웹 서비스
+
+    // 1.
+    //@ResponseBody // 응답객체 자동 지원 [ 단 해당 클래스가 @RestController 사용했을떄 생략 가능
+    @PostMapping("")
+    public boolean doPost( @RequestBody TodoDto todoDto ) { // 요청 매개변수 : 입력받은 정보들 [Dto]
+                            // @RequestBody : HTTP BODY( post , put ) json 형식으로 요청 매핑
+        System.out.println("TodoController.doPost");
+        System.out.println("todoDto = " + todoDto);
+
+        return todoService.doPost(todoDto);
+
+    }
+
+    // 2.
+    @GetMapping("")
+    public List<TodoDto> doGet(){ // 요청 매개변수 : 출력에 필요한 정보들 [ x ]
+        System.out.println("TodoController.doGet");
+        return todoService.doGet();
+    }
+
+    // 3.
+    @PutMapping("")
+    public boolean doPut( @RequestBody  TodoDto todoDto ){ // 요청 매개변수 : 수정에 필요한 정보들 [ Dto ]
+                            
+        System.out.println("TodoController.doPut");
+        System.out.println("todoDto = " + todoDto);
+        return todoService.doPut(todoDto);
+    }
+
+    // 4.
+    @DeleteMapping("")
+    public boolean doDelete( @RequestParam int tno ){ // 요청 매개변수 : 삭제에 필요한 정보들 [int]
+                                // @RequestParam : 쿼리스트링 에서의 매개변수 요청 매핑
+        System.out.println("TodoController.doDelete");
+        System.out.println("tno = " + tno);
+        return todoService.doDelete(tno);
+    }
+
+    //5.HTML 반환하는 매핑주소 만들기
+    @GetMapping("/index")
+    public Resource getIndex(){
+        return new ClassPathResource("templates/todo.html");
+    }
+    // Rsource : org.springframework.core.io.Resource;
+    // ClassPathResource : org.springframework.core.io.ClassPathResource;
+
+}
